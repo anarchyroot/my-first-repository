5ec72cb9962b9ef18f4b11145d5c22050135a326
Matthew Altomare <matt@shotbymatt.com>
2017-05-11 01:25:36 +0100

    Created extra pages with index template

--- Desktop/portfolio2/js/bootstrap2.js
+++ Desktop/portfolio2/js/bootstrap2.js
@@ -15,3 +15,39 @@ $(function(){
             $('b', this).toggleClass("caret caret-up");
         });
 });
+
+$(document).ready(function(){
+    var i = -1;
+    function nextMsg() {
+        if(i === messages.length - 1){
+            i = -1;
+        }
+
+        i++;
+
+        $('#home-splash-content-text').html(messages[i]).fadeIn(500).delay(times[i]).fadeOut(500, nextMsg);
+
+
+    }
+// list of messages to display
+    var messages = [
+        "Shot By Matt",
+        "SHOTBYMATT.COM",
+        "PHOTOHGRAPHY BY:",
+        "MATTHEW ALTOMARE",
+    ];
+
+//list of times
+    var times = [
+        1000,
+        1000,
+        1000,
+        1000
+    ];
+
+// initially hide the message
+    $('#home-splash-content-text').hide();
+
+// start animation
+    nextMsg();
+});
